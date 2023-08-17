f88c5ce6ab7db74668080fdf5261ff9e19cb3fb2
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-08-17 21:06:06 +0530

    resolved warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {  createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Error from './components/Error';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
